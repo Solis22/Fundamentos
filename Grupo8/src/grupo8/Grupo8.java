6889a19d4a699bc99cc2e98efe3d79b140b10384
Sebastian <39276216+Solis22@users.noreply.github.com>
2018-06-04 13:13:03 -0600

    Grupo 8 Ejercicio

---
+++ Grupo8/src/grupo8/Grupo8.java
@@ -0,0 +1,30 @@
+
+package grupo8;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Grupo8 {
+
+   
+    public static void main(String[] args) 
+{
+        Random random = new Random();
+        for(int counter=1; counter<=8;counter++){
+            System.out.println("Los resultados del grupo " + counter + " son");
+            int resA;
+            resA = 1+random.nextInt(4);
+            int resB;
+            resB = 1+random.nextInt(4);
+            System.out.println("El resultado del partido A contra B fue " + "A " + resA + " - " + resB + " B");
+            int resC;
+            resC = 1+random.nextInt(4);
+            int resD;
+            resD = 1+random.nextInt(4);
+            System.out.println("El resultado del partido C contra D fue " + "C " + resC + " - " + resD + " D");
+            System.out.println(" ");
+
+        }
+
+    }
+}
