6697ccf345589b9cfb881805f0e2a74d0d93428b
Sebastian <39276216+Solis22@users.noreply.github.com>
2018-06-12 20:13:00 -0600

    Ambos programas estan!

---
+++ Clase/11 de junio/Cuadrado/src/cuadrado/Cuadrado1.java
@@ -0,0 +1,13 @@
+
+package Cuadrado;
+
+public class Cuadrado1 {
+    
+    public static void main(String[]args){
+        Cuadrado square= new Cuadrado(5);
+        square.Drawsquare();
+        
+        Cuadrado square2= new Cuadrado (15);
+        square2.Drawsquare();
+    }
+}
